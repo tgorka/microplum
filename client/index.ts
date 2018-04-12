b4e5b282b03f89e8fb0f38edf97e47d96de40f9b
tgorka <tomasz.gorka@carmoov.io>
2018-04-12 18:02:46 +0200

    support for debug and source-map, change env to node8, change target to es2017

--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,4 @@
+import * as _debug from "debug";
 import Microplum from "..";
 
 const appName = process.env.APP_NAME || "microplum";
@@ -5,6 +6,9 @@ const version = parseInt(process.env.VERSION || "1");
 const roles = (process.env.ROLES || "").split(",");
 const ampqUrl = process.env.AMQP_URI || process.env.CLOUDAMQP_URL || "amqp://localhost/microplum";
 const debugUserId = process.env.DEBUG_USER_ID || null;
+
+const debug = _debug("microplum");
+const info = _debug("microplum-info");
 
 const init = () => {
     const microplum = new Microplum({
@@ -19,9 +23,15 @@ const init = () => {
 };
 
 export const act = async (pin: any): Promise<any> => {
+    info(`[microplum][CALL]<<{role:${pin.role},cmd:${pin.cmd}...}`);
+    debug(`[microplum][CALL]<<${JSON.stringify(pin)}`);
     const microplum = init();
+    debug("[microplum] client initialized");
     const result = await microplum.actPromise(pin);
+    debug(`[microplum][RESULT]>>${JSON.stringify(result)}`);
     microplum.close();
+    debug("[microplum] closed");
+    info(`[microplum][RESULT]>>success`);
     return result;
 };
 export default act;
--- handler.ts
+++ handler.ts
@@ -1,3 +1,4 @@
+import "source-map-support/register";
 import * as jsonpath from "jsonpath";
 import microplumCall from "./client";
 
